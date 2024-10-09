e62ce0161f43e5bd7f3dfebe81ea2f563a902a89
Pál Máté <palmate2004@gmail.com>
2024-10-09 09:34:31 +0200

    Made it more compact with comments

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,24 +1,18 @@
 package hu.bme.mit.spaceship;
-
 import java.util.Random;
-
+// less whitespaces to make it more compact
 /**
 * Class storing and managing the torpedoes of a ship
 *
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
   private int torpedoCount = 0;
-
   private Random generator = new Random();
-
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
@@ -29,17 +23,13 @@ public class TorpedoStore {
       }
     }
   }
-
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
     }
-
     boolean success = false;
-
     // simulate random overheating of the launcher bay which prevents firing
     double r = generator.nextDouble();
-
     if (r >= FAILURE_RATE) {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
@@ -48,14 +38,11 @@ public class TorpedoStore {
       // simulated failure
       success = false;
     }
-
     return success;
   }
-
   public boolean isEmpty(){
     return this.torpedoCount <= 0;
   }
-
   public int getTorpedoCount() {
     return this.torpedoCount;
   }
